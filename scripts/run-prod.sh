471109aec50f3d534d6dd686271cdef235236518
Thomas Ridings <thomas.a.ridings@gmail.com>
2018-11-30 22:41:13 +0000

    Extending the timeout

--- scripts/run-prod.sh
+++ scripts/run-prod.sh
@@ -2,6 +2,6 @@
 # ps -ax | grep -i jupyter
 # When deployed use the following
 # sudo systemctl start jupyterhub.service
-sudo touch var/log/jupyter.log
-sudo chown ubuntu:ubuntu var/log/jupyter.log
+sudo touch /var/log/jupyter.log
+sudo chown ubuntu:ubuntu /var/log/jupyter.log
 jupyterhub -f jupyterhub_config.py --debug >> /var/log/jupyter.log 2&>1
