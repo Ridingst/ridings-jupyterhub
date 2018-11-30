6e83960a317f33bfdcf3fd5ba6be4e86dc785223
Thomas Ridings <thomas.a.ridings@gmail.com>
2018-11-30 08:59:07 +0000

    Adding some management functions

--- run-prod.sh
+++ run-prod.sh
@@ -1,1 +1,3 @@
-nohup ./run.sh 0<&- &> /var/log/jupyterhub.log
+# Incase you loose it
+# ps -ax | grep -i jupyter
+nohup jupyterhub -f jupyterhub_config.py >> /var/log/jupyterhub.log 2>&1 & echo $! > jupyterhub.pid
